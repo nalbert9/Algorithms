2bdd2d9654aaf754a2bfce0fe8b7b047352471bc
Badji N'Wuitcha <nalbert9@hotmail.fr>
2021-09-15 09:11:11 +0200

    Added steps

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
+"""
+The binary_search function takes a sorted array and an item. If the
+item is in the array, the function returns its position.
+"""
 def binary_search(list_elements, item):
     low = 0
     high = len(list_elements)
 
     while (low <= high):
+        # Each time, you check the middle element
         middle = (low + high)//2
         guess = list_elements[middle]
 
+        # If the guess is too low, you update low
+        # And if the guess is too high, you update high
         if (guess == item):
             return middle
         elif (guess > item):
             high = middle - 1
         else:
             low = middle + 1
+
+        # Running time O(log n)
     return None
 
 if __name__=='__main__':
