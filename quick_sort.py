c9aed2e5d87e196d85644454ba68a30d572b9cf1
Badji N'Wuitcha <nalbert9@hotmail.fr>
2021-09-23 07:09:18 +0200

    Added file

---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+"""
+Quicksort is a sorting algorithm. It’s much faster than selection sort
+and is frequently used in real life (C standard library function qsort).
+Quicksort use Divide and conquer.
+"""
+def quick_sort(array):
+    return 
+
+if __name__ =='__main__':
+    example = [27, 41, 66, 28, 44, 78, 87, 19, 31, 76, 58, 88, 83, 97, 12,99 ]
+    print(quick_sort)
